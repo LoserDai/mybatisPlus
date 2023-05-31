4552d8279130e7c4bd07444c31d04d1e725863be
LoserDai <15570237906@139.com>
2023-05-31 14:02:16 +0800

    尝试把mysql的数据导入到redis

--- api-bankend/src/main/java/com/df/config/RedisConfig.java
+++ api-bankend/src/main/java/com/df/config/RedisConfig.java
@@ -1,13 +1,13 @@
 package com.df.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
@@ -17,32 +17,26 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  * @Package_Name com.df.config
  */
 @Configuration
+@EnableCaching
 public class RedisConfig {
+
     @Bean
-    public RedisTemplate<String,Object> getRedisTemplate(RedisConnectionFactory factory){
-        //创建 RedisTemplate
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
 
-        //创建 Redis 中的 key 的序列化器
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        //创建 Redis 中的 value 的序列化器
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        jackson2JsonRedisSerializer.setObjectMapper(new ObjectMapper());
 
-        ObjectMapper om = new ObjectMapper();
-        // 指定要序列化的field,get和set,以及修饰符范围，ANY是都有包括private和public
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        RedisSerializer stringSerializer = new StringRedisSerializer();
 
-        //得到的JSON串的值中带有对象的类型，指定序列化输入的类型必须是非final修饰的
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-
-        jackson2JsonRedisSerializer.setObjectMapper(om);
-        //设置key:value和hashKey:hashValue的序列化的方式
-        template.setKeySerializer(stringRedisSerializer);
+        template.setKeySerializer(stringSerializer);
         template.setValueSerializer(jackson2JsonRedisSerializer);
-        template.setHashKeySerializer(stringRedisSerializer);
+
+        template.setHashKeySerializer(stringSerializer);
         template.setHashValueSerializer(jackson2JsonRedisSerializer);
 
         return template;
     }
+
 }
